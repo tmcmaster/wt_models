46ef6383a76c71ff7082bb654b147e03b48f71bc
Tim McMaster <tim@mcmaster.id.au>
2022-11-05 23:27:32 +1100

    added model OrderSupport

--- lib/src/title_id_json_support.dart
+++ lib/src/title_id_json_support.dart
@@ -1,4 +1,10 @@
 import 'package:wt_models/src/id_json_support.dart';
 import 'package:wt_models/src/title_support.dart';
 
+abstract class OrderTitleIdJsonSupport<T> extends TitleIdJsonSupport<T> with OrderSupport {}
+
 abstract class TitleIdJsonSupport<T> extends IdJsonSupport<T> with TitleSupport {}
+
+abstract class OrderSupport {
+  double getOrder();
+}
