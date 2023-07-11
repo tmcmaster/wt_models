82c2d986774388415f6dac5415e3577ddb3649b6
Tim McMaster <tim@mcmaster.id.au>
2023-07-12 09:16:52 +1000

    moved BaseModel one step closer to being a mixin

--- lib/src/base_model.dart
+++ lib/src/base_model.dart
@@ -1,7 +1,11 @@
 import 'package:wt_models/src/csv_support.dart';
-import 'package:wt_models/src/id_json_support.dart';
+import 'package:wt_models/src/id_support.dart';
+import 'package:wt_models/src/json_support.dart';
 import 'package:wt_models/src/title_support.dart';
 import 'package:wt_models/src/type_support.dart';
 
-abstract class BaseModel<T> extends IdJsonSupport<T>
-    with TitleSupport, TypeSupport<T>, CsvSupport {}
+// TODO: need to look at converting this to a mixing, considering the migration of dependent external code.
+abstract class BaseModel<T>
+    with IdSupport<T>, JsonSupport, TitleSupport, TypeSupport<T>, CsvSupport {
+  const BaseModel();
+}
